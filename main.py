960a52b77c4442c59f9636a587d5116f4138133f
Up-Level <charliewaltham2@gmail.com>
2024-12-30 19:49:40 +0000

    Closer to functional

--- main.py
+++ main.py
@@ -16,64 +16,62 @@ def cart2polar(x, y, z):
 
     return r, theta, phi
 
-for i in range(50, 51):
-    earth_radius = 6371
+earth_radius = 6371
+
+wic_pixels = data["WIC_PIXELS"]
+# Ensure all values >0 for log
+wic_pixels = np.vectorize(lambda p: 1 if p < 1 else p)(wic_pixels)
+
+fov = np.deg2rad(data["VFOV"])
+fov_vector = np.array([
+    np.cos(fov) * np.sin(fov),
+    np.sin(fov),
+    np.cos(fov) ** 2
+])
+
+for i in range(wic_pixels.shape[0]):
     pos = np.array([data["ORB_X"][i], data["ORB_Y"][i], data["ORB_Z"][i]])
-    pos = np.array([0, 7000, 20000])
-    data["RADIUS"][i] = 3.139
+    #pos = np.array([0, 7000, 20000])
 
-    centre_pos = pos / data["RADIUS"][i]
-    dir_vector = centre_pos / np.sqrt(np.dot(centre_pos, centre_pos))
+    # c is constant per pixel
+    c = np.dot(pos, pos) - (earth_radius ** 2)
 
-    centre_dir = np.array([
-        np.arcsin(dir_vector[2]),
-        np.arctan2(dir_vector[0], dir_vector[1])
-    ])
-    """centre_dir = np.array([
-        np.arctan2(centre_pos[1], centre_pos[0]),
-        np.arccos(centre_pos[2] / (data["RADIUS"][i] * earth_radius)),
-    ])"""
+    centre_dir = -pos / np.sqrt(np.dot(pos, pos))
     
-    pixel_data = data["WIC_PIXELS"][i]
-    pixel_locs = np.zeros((pixel_data.shape[0], pixel_data.shape[1], 3))
+    current_image = wic_pixels[i]
+    pixel_locs = np.zeros((current_image.shape[0], current_image.shape[1], 3))
 
-    fov = np.deg2rad(data["VFOV"])
-    fov_vector = np.array([
-        np.cos(fov) * np.sin(fov),
-        np.sin(fov),
-        np.cos(fov) ** 2
-    ])
-    print(fov_vector)
-
-    for x in range(pixel_data.shape[0]):
-        for y in range(pixel_data.shape[1]):
-            pixel_offset = (np.array([x / pixel_data.shape[0], y / pixel_data.shape[1]]) - 0.5) * fov
-            angles = pixel_offset + centre_dir
-
-            direction = -np.array([
-                np.sin(angles[0]) * np.cos(angles[1]),
-                np.cos(angles[0]) * np.cos(angles[1]),
-                np.sin(angles[0]),
-            ])
+    for x in range(current_image.shape[0]):
+        for y in range(current_image.shape[1]):
+            pixel_offset = np.array([x / current_image.shape[0] - 0.5, 1, y / current_image.shape[1] - 0.5])
+            direction = pixel_offset * fov_vector + centre_dir
 
             a = np.dot(direction, direction)
             b = 2 * np.dot(pos, direction)
-            c = np.dot(pos, pos) - (earth_radius ** 2)
 
-            #print(a, b, c)
-            #print(direction, pos, centre_pos, np.rad2deg(angles), np.rad2deg(centre_dir), b)
-            #print(b ** 2, 4 * a * c)
+            #print(pixel_offset, direction, centre_dir, pos, centre_pos)
 
             discriminant = (b ** 2) - (4 * a * c)
             if discriminant >= 0:
-                roots = np.roots((a, b, c))
-                pixel_locs[x][y] = pos + roots[1] * direction
-                print(pixel_locs[x][y])
+                #roots = np.roots((a, b, c))
+                root = (-b + np.sqrt(discriminant)) / (2 * a)
+                pixel_locs[x][y] = pos + root * direction
+                #print(pixel_locs[x][y])
+                
+    image_locs = ((pixel_locs - pixel_locs.min()) / (pixel_locs.max() - pixel_locs.min() + 0.00001) * 255).astype(int)
+    image_locs = image_locs.reshape((image_locs.shape[0], image_locs.shape[1] * image_locs.shape[2]))
 
-    """image = (pixel_locs.min() + pixel_locs * (np.abs(pixel_data.min()) + pixel_data.max())) * 255
-    #image = (255 * (pixel_data / pixel_data.max())).astype(int)
-    print(image.min(), image.max(), image.shape)
+    image = ((current_image - current_image.min()) / (current_image.max() - current_image.min() + 0.00001) * 255).astype(int)
 
-    writer = png.Writer(256, 256, greyscale=True)
-    with open("out.png", "wb") as file:
-        writer.write(file, image.tolist())"""
+    print(f"""{i}
+locs: {pixel_locs.min()} {pixel_locs.max()} image locs: {image_locs.min()} {image_locs.max()}
+wic: {current_image.min()} {current_image.max()} image wic: {image.min()} {image.max()}
+""")
+
+    writer = png.Writer(256, 256, greyscale=False)
+    writer_gs = png.Writer(256, 256, greyscale=True)
+
+    with open(f"locs/{i}.png", "wb") as file:
+        writer.write(file, image_locs.tolist())
+    with open(f"images/{i}.png", "wb") as file:
+        writer_gs.write(file, image.tolist())
